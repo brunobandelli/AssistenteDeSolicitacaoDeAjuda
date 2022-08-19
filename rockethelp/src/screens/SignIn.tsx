e7182b06e9d0f6f6f96702918f99c899f8cedbf3
brunobandelli <brunobandelli@gmail.com>
2022-08-19 01:02:07 -0300

    Autenticação de Signin e Logout.

--- rockethelp/src/screens/SignIn.tsx
+++ rockethelp/src/screens/SignIn.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useState } from "react";
+import { Alert } from "react-native";
+import auth from "@react-native-firebase/auth";
 import { VStack, Heading, Icon, useTheme } from "native-base";
 import { Envelope, Key } from "phosphor-react-native";
 import Logo from "../assets/logo_primary.svg";
@@ -7,13 +9,40 @@ import { Input } from "../components/Input";
 import { Button } from "./../components/Button";
 
 export function SignIn() {
-  const [name, setName] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const { colors } = useTheme();
 
   function handleSignIn() {
-    console.log(name, password);
+    if (!email || !password) {
+      return Alert.alert("Entrar", "Informe e-mail e senha.");
+    }
+
+    setIsLoading(true);
+
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      // .then((response) => {
+      //   console.log(response);
+      // })
+      .catch((error) => {
+        console.log(error.code);
+        setIsLoading(false);
+
+        if (error.code === "auth/invalid-email") {
+          return Alert.alert("Entrar", "E-mail inválido.");
+        }
+        if (error.code === "auth/wrong-password") {
+          return Alert.alert("Entrar", "E-mail ou senha inválida.");
+        }
+        if (error.code === "auth/user-not-found") {
+          return Alert.alert("Entrar", "E-mail ou senha inválida.");
+        }
+
+        return Alert.alert("Entrar", "Não foi possível acessar");
+      });
   }
 
   return (
@@ -29,7 +58,7 @@ export function SignIn() {
         InputLeftElement={
           <Icon as={<Envelope color={colors.gray[300]} />} ml={4} />
         }
-        onChangeText={setName}
+        onChangeText={setEmail}
       />
       <Input
         mb={8}
@@ -39,7 +68,12 @@ export function SignIn() {
         onChangeText={setPassword}
       />
 
-      <Button title="Entrar" w="full" onPress={handleSignIn} />
+      <Button
+        title="Entrar"
+        w="full"
+        onPress={handleSignIn}
+        isLoading={isLoading}
+      />
     </VStack>
   );
 }
